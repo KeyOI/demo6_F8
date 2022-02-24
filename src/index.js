f1992576c149fb3bbf45c763751b32f8890340cb
ledangtuankhoi2 <ledangtuankhoi2@gmail.com>
2022-02-24 02:47:32 -0500

    test husky

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.json());
+                                        app.use(express.json());
 
 app.use(
     express.urlencoded({
