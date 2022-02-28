c4a93c0e21d4670c73f7a00c50150ea0532679ae
ledangtuankhoi2 <ledangtuankhoi2@gmail.com>
2022-02-28 03:20:02 -0500

    test husky

--- src/app/controllers/APIController.js
+++ src/app/controllers/APIController.js
@@ -3,12 +3,12 @@ const { mutipleMongooseToObject } = require('../../util/mongoose');
 
 class APIController {
     // [GET] /
-    courses(req, res,                                  next) {
+    courses(req, res,                                    next) {
         Course.find({})
             .sort({ updatedAt: 'desc' })
             .then((courses) => {
                 res.json(courses);
-            })
+            })  
             .catch(next);
     }
 }
